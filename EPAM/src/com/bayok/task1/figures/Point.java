0d0351822f23c3dba0f069ad9341157c674a3d37
BayokArtyom <artyom_bayok@epam.com>
2017-06-28 11:12:24 +0300

    changed to private

--- EPAM/src/com/bayok/task1/figures/Point.java
+++ EPAM/src/com/bayok/task1/figures/Point.java
@@ -2,9 +2,9 @@ package com.bayok.task1.figures;
 
 public class Point {
 
-    public int x;
-    public int y;
-    public int z;
+    private int x;
+    private int y;
+    private int z;
 
     public void Point(int x, int y, int z) {
         this.x = x;
@@ -12,27 +12,27 @@ public class Point {
         this.z = z;
     }
 
-    public int getX() {
+    private int getX() {
         return x;
     }
-    public void setX(int x) {
+    private void setX(int x) {
         this.x = x;
     }
-    public int getY() {
+    private int getY() {
         return y;
     }
-    public void setY(int y) {
+    private void setY(int y) {
         this.y = y;
     }
-    public int getZ() {
+    private int getZ() {
         return z;
     }
-    public void setZ(int z) {
+    private void setZ(int z) {
         this.z = z;
     }
 
     @Override
-    public String toString() {
+    private String toString() {
         return "(" + x + ", " + y + ", " + z +")";
     }
 
